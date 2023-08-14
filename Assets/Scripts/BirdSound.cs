6d662972763b4028b0f6abc0bc07cb0ecc283a36
3RFUNn <68325981+3RFUNn@users.noreply.github.com>
2023-08-14 23:47:25 +0330

    Update BirdSound.cs

--- Assets/Scripts/BirdSound.cs
+++ Assets/Scripts/BirdSound.cs
@@ -1,11 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
-
-using System.Collections;
-using UnityEngine;
-
 public class BirdSound : MonoBehaviour
 {
     [SerializeField] private AudioClip[] audioClips;
